62735e4a5e92064e1b6f795432f1ce7fe6329473
GWilman <georgewilman1@gmail.com>
2017-10-10 12:42:31 +0100

    added rules

--- js/app.js
+++ js/app.js
@@ -58,7 +58,7 @@ $(() => {
   });
 
   function prepareGame() {
-    $('.intro, .timeUp, .level2Info, .cloudLeft, .cloudRight').css({'display': 'none'});
+    $('#rules, .intro, .timeUp, .level2Info, .cloudLeft, .cloudRight').css({'display': 'none'});
     $letterSpace.css({'display': 'block'});
     $scoreboard.css({'display': 'block'});
     score = 0;
@@ -208,6 +208,8 @@ $(() => {
     $scoreCount.html(score);
     if (currentLevel === 1 && score >= 15) {
       currentLevel = 2;
+    } else if (currentLevel === 2 && score >= 15) {
+      currentLevel = 3;
     }
   }
 
@@ -250,41 +252,41 @@ e.g. collision logic.
 Plan of Action
 
 1. Begin by cracking the key functions of the game:
-    - check an entered word vs a list of English words to return a value of true or false
-    - check an entered word vs the lettersInPlay array
-    - create method for adding and removing letters from the array
+- check an entered word vs a list of English words to return a value of true or false
+- check an entered word vs the lettersInPlay array
+- create method for adding and removing letters from the array
 
 2. Create the first level, the MVP.
-    - design layout of game screen
-    - add timer and score features
-    - implement game logic defined above
+- design layout of game screen
+- add timer and score features
+- implement game logic defined above
 
 3. Work on subsequent levels and extra functionality.
 
-    Level structure v1 (20secs per level)
-    Increase speed of letter disappearance each level, change weather.
-    - Level 1 - Score more than X points (basic game)
-    - Level 2 - Score more than X points using only green letters
-    - Level 3 - Score more than X points using only capital letters (introduce lower case)
-    - Level 4 - Score more than X points, words under 4 letters long not allowed
-    - Level 5 - Score more than X points using only green, capital letters, no words under
-                5 letters long allowed.
+Level structure v1 (20secs per level)
+Increase speed of letter disappearance each level, change weather.
+- Level 1 - Score more than X points (basic game)
+- Level 2 - Score more than X points using only green letters
+- Level 3 - Score more than X points using only capital letters (introduce lower case)
+- Level 4 - Score more than X points, words under 4 letters long not allowed
+- Level 5 - Score more than X points using only green, capital letters, no words under
+5 letters long allowed.
 
 
 Basic Game Logic:
-  - Instructions dictate level rules and win conditions.
-  - On play, start timer.
-  - On start timer, remove instructions, move title to top left (small), generate letters.
-  - Letters appear in random screen location and fade out (6 secs), random letter is added to
-  lettersInPlay array.
-  - User enters word and presses enter.
-  - Submitted word is checked against valid word list (array? txt file?).
-  - Letters of submitted word are checked to make sure they are present in lettersInPlay array.
-  - If both of the above return true, score is updated.
-  - A green shadow/border is added to letters on screen.
-  - This cycle repeats until timer runs out.
-  - If score === win conditions, player passes the level and can move on to the next.
-  - If score !== win conditions, player is asked to repeat level.
+- Instructions dictate level rules and win conditions.
+- On play, start timer.
+- On start timer, remove instructions, move title to top left (small), generate letters.
+- Letters appear in random screen location and fade out (6 secs), random letter is added to
+lettersInPlay array.
+- User enters word and presses enter.
+- Submitted word is checked against valid word list (array? txt file?).
+- Letters of submitted word are checked to make sure they are present in lettersInPlay array.
+- If both of the above return true, score is updated.
+- A green shadow/border is added to letters on screen.
+- This cycle repeats until timer runs out.
+- If score === win conditions, player passes the level and can move on to the next.
+- If score !== win conditions, player is asked to repeat level.
 
 XMLHttpRequest cannot load file:///Users/georgewilman/Development/WDI_PROJECT_1/words.txt. Cross origin requests are only supported for protocol schemes: http, data, chrome, chrome-extension, https.
 
